368eefb7487b3e32904b0695f2cdba1862ba820c
gioia <m.puddu86@studenti.unica.it>
2025-10-07 16:23:05 +0200

    importiamo analisi preliminare

---
+++ Data Analysis/analisi-preliminare.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np
+
+from Data.dataset import Dataset
+
+
+def statistiche_generali(df: Dataset):
+
+    print('='*80, "ANALISI GENERALE DEL DATASET", '='*80, sep='\n', end='\n\n')
+
+    shp = df.dataset.shape
+    print(f'Il dataset ha {shp[0]} record da {shp[1]} attributi ciascuno\n\n')
+
+
+    print('Di seguito il numero di valori nulli per attributo nel dataset:', df.dataset.isnull().sum(), sep='\n', end='\n\n')
+
+    print('Di seguito il il tipo di dato di ciascun attributo nel dataset:', df.dataset.dtypes, sep='\n', end='\n\n')
+    
+    num_attributi = df.dataset['divorced'].value_counts()
+    print(f'Il dataset ha {num_attributi[0]} attributi per la classe 0 e {num_attributi[1]} attributi per la classe 1')
+    
+
