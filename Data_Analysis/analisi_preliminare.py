3fa3655373428221f05effc184decaebfe53cbf2
gioia <m.puddu86@studenti.unica.it>
2025-10-08 10:02:41 +0200

    aggiunte scritte di completamento

--- Data_Analysis/analisi_preliminare.py
+++ Data_Analysis/analisi_preliminare.py
@@ -11,7 +11,6 @@ def statistiche_generali(df: Dataset):
     shp = df.dataset.shape
     print(f'Il dataset ha {shp[0]} record da {shp[1]} attributi ciascuno\n\n')
 
-
     print('Di seguito il numero di valori nulli per attributo nel dataset:', df.dataset.isnull().sum(), sep='\n', end='\n\n')
 
     print('Di seguito il il tipo di dato di ciascun attributo nel dataset:', df.dataset.dtypes, sep='\n', end='\n\n')
@@ -19,4 +18,6 @@ def statistiche_generali(df: Dataset):
     num_attributi = df.dataset['divorced'].value_counts()
     print(f'Il dataset ha {num_attributi[0]} attributi per la classe 0 e {num_attributi[1]} attributi per la classe 1')
     
+    print("\n" + "="*80)
+    print("Analisi preliminare completata con successo\n")
 
